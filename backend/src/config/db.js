5ad391faf9cb19c4882464966ba591e426bc0d4b
Ayush Thapa <aayushmagar715@gmail.com>
2026-03-11 20:57:26 +1100

    fix database url for railway postgresql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,18 @@
-
-import {Pool} from "pg"; //pg doesn't have named exxports, so we destructuer from default
+import { Pool } from "pg";
 
 const pool = new Pool({
-    host: process.env.DB_HOST,    // Database host(e.g. localhost)
-    user: process.env.DB_USER,    // Database username
-    password: process.env.DB_PASSWORD,  // Dababse password
-    database: process.env.DB_NAME,   // Name of the habit tracker database
-    port: process.env.DB_PORT,     // Default is usually 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // required for Railway PostgreSQL
+    }
 });
 
-
-//Test the connection on startup
-pool.connect((err)=> {
-if (err) {
-    console.error('Database connection failed:', err.message);
-
-} else{
-    console.log('Connected to PostgreSQL');
-}
+pool.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.message);
+    } else {
+        console.log('Connected to PostgreSQL');
+    }
 });
 
 export default pool;
