50808ffb396f36e3b020fda1baf7e44d4579c70a
Ayush Thapa <aayushmagar715@gmail.com>
2026-03-14 18:20:45 +1100

    Fix screenshot paths and filenames

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import {rateLimit} from 'express-rate-limit';
 import authRoutes from './routes/auth.route.js';
 import habitRoutes from './routes/habit.route.js';
 const app = express();
-app.set('trust proxy',1);
+app.set('trust proxy',true);
 // Security headers
 app.use(helmet());
 
